b1bec9e72f6f008cdaded4dc9c8de9e19279d61f
x-markthespot <markyrasonabe2405@gmail.com>
2024-10-03 13:32:48 +0800

    Initial commit and still improving the code

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("hello Kurasoy");
-console.log("Igop kayu ang nasa likod nako");
+//Input
+const person1 ={
+    name:"Mark Lester G. Rasonabe",
+    age:19,
+    occupation:"Student",
+    status:"Undefined"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
+console.log("Status: "+person1.status);
